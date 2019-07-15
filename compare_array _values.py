32235f094b85bba63e55835fa468a9e625f3f72b
krist96 <30568877+krist96@users.noreply.github.com>
2019-07-15 20:01:41 +0200

    Add files via upload

---
+++ compare_array _values.py
@@ -0,0 +1,21 @@
+class My(object):
+    def __init__(self, tab, string):
+        self.tab = tab
+        self.string = string
+
+    def task2(self):
+        tab = self.tab #Tworzymy wewnetrzne zmienną tablicową
+        n = self.string #Tworzymy wewnetrzne zmieną z wartością informacyjną o ostatnim elemencie
+        tab.sort() #sortujemy wejściową tabelę
+
+        ##Funkcja Sprawdzająca, i wypisująca brakujące elementy talicy
+        def rek(i):   
+            if(i <= n): #Sprawdzamy czy i jest równe 10
+                if (tab.count(i) < 1):
+                    print(i)
+                rek(i+1)  #Rekurencyjnie wywołujemy funkcję z wartością podniesioną o jeden
+        ##Wywołujemy powyższą funkcję
+        rek(i=1)
+
+my = My([2,3,7,4,9], 10)
+my.task2()
